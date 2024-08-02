cbc89a7835b5808313145cc9f7d2c13e5ebe2bcf
TutorialMan7727 <157202119+TutorialMan7727@users.noreply.github.com>
2024-08-02 11:09:50 +0100

    Lets try updating it again and hope it wworks

--- src/puppeteer-extra-plugin-capsolver.d.ts
+++ src/puppeteer-extra-plugin-capsolver.d.ts
@@ -1,12 +1,12 @@
-import { Browser, Page } from "puppeteer";
-
 export = puppeteer_extra_plugin_capsolver;
 
 declare class puppeteer_extra_plugin_capsolver extends PuppeteerExtraPlugin {
   constructor(apiKey: string = null, verbose: number = 0);
 
+  get name(): string;
+  get defaults(): PluginOptions;
+
+  onBrowser(browser: Puppeteer.Browser, opts: any): Promise<void>;
+  onPageCreated(page: Puppeteer.Page): Promise<void>;
   onPluginRegistered(): Promise<void>;
-  onBrowser(browser: Browser): Promise<void>;
-  onPageCreated(page: Page): Promise<void>;
-  _addSolverToPage(page: Page): void;
 }
